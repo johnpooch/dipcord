07b1224653e2597faa46d9bb7846ec9262f3dc88
= <=>
2024-06-10 21:52:04 +0100

    working on create-game

--- src/commands/diplicity/create-game.ts
+++ src/commands/diplicity/create-game.ts
@@ -1,7 +1,108 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 
 import * as api from '../../diplicity';
-import { CommandInteraction } from 'discord.js';
+import {
+  ChannelType,
+  Colors,
+  CommandInteraction,
+  PermissionFlagsBits,
+} from 'discord.js';
+import { createScopedLogger } from '../../util/telemetry';
+import { createDiscordForm } from '../../util/form';
+// import { DiscordFormConfig } from '../../util/form';
+
+const commandResponseContent = `
+# Create a new Diplicity game
+
+You are about to create a new Diplomacy game in this server. This will authorize the bot to create new roles for each player, and create new channels in a category to manage the game.
+
+You will be able to add new players after the game has been initiated.
+`;
+
+const submitResponseContent = `
+# Game created
+
+The game has been created successfully. You can now add players to the game by using the \`/add-player\` command.
+`;
+
+const log = createScopedLogger('commands/game/create-game');
+
+type FormData = {
+  variant: string;
+  phaseLength: string;
+};
+
+const defaultValues: FormData = {
+  variant: 'classical',
+  phaseLength: '24',
+};
+
+const createGameForm = createDiscordForm<FormData>({
+  initialValues: defaultValues,
+  interactionContent: commandResponseContent,
+  fields: [
+    {
+      type: 'select',
+      name: 'variant',
+      placeholder: 'Variant',
+      options: [
+        { label: 'Classical', value: 'classical' },
+        { label: 'Modern', value: 'modern' },
+      ],
+    },
+    {
+      type: 'select',
+      name: 'phaseLength',
+      placeholder: 'Phase length',
+      options: [
+        { label: '48 hours', value: '48' },
+        { label: '24 hours', value: '24' },
+        { label: '12 hours', value: '12' },
+      ],
+    },
+  ],
+  onSubmit: async (interaction, values) => {
+    await interaction.update({
+      content: `Creating game with variant ${values.variant} and phase length ${values.phaseLength} hours`,
+      components: [],
+    });
+
+    // TODO create game
+
+    // Create a role in the guild called "England"
+    const role = await interaction.guild.roles.create({
+      name: 'England',
+      color: Colors.Red,
+    });
+
+    // Create a channel in the guild called "England - Orders" with the category "Orders"
+    const category = await interaction.guild.channels.create({
+      name: 'Orders',
+      type: ChannelType.GuildCategory,
+    });
+
+    await interaction.guild.channels.create({
+      name: 'england-orders',
+      type: ChannelType.GuildText,
+      parent: category.id,
+      // channel should be hidden from everyone that doesn't have the "england" role
+      permissionOverwrites: [
+        {
+          id: interaction.guild.roles.everyone.id,
+          deny: PermissionFlagsBits.ViewChannel,
+        },
+        {
+          id: role.id,
+          allow: PermissionFlagsBits.ViewChannel,
+        },
+      ],
+    });
+
+    await interaction.editReply({
+      content: submitResponseContent,
+    });
+  },
+});
 
 const data = new SlashCommandBuilder()
   .setName('create-game')
@@ -9,11 +110,54 @@ const data = new SlashCommandBuilder()
 
 const execute = async (interaction: CommandInteraction): Promise<void> => {
   const { user, channelId } = interaction;
+
+  log.info(
+    `Command invoked: ${interaction.commandName}; user: ${user.id}; channelId: ${channelId}`,
+  );
+
   try {
     const { token: botToken } = await api.login();
+    log.info('Bot token acquired');
+
     const { token: userToken } = await api.getUserToken(user.id, botToken);
-    const game = await api.createGame(channelId, userToken);
-    await interaction.reply(`Game created with ID: ${game.id}`);
+    log.info('User token acquired');
+
+    const stagingGames = await api.listGames('Staging', userToken);
+    log.info('Staging games retrieved for user');
+
+    const stagingGameForServer = stagingGames.find(
+      (game) => game.name === channelId,
+    );
+
+    if (stagingGameForServer) {
+      log.info(
+        'There is already a staging game for this server. Responding to user.',
+      );
+      await interaction.reply(
+        'Cannot create a new game. There is already a staging game for this server.',
+      );
+      return;
+    }
+
+    const startedGames = await api.listGames('Started', userToken);
+    log.info('Started games retrieved for user.');
+
+    const startedGameForServer = startedGames.find(
+      (game) => game.name === channelId,
+    );
+
+    if (startedGameForServer) {
+      log.info(
+        'There is already an active game for this server. Responding to user.',
+      );
+      await interaction.reply(
+        'Cannot create a new game. There is already an active game for this server.',
+      );
+      return;
+    }
+
+    log.info('Responding to user with create game form');
+    await createGameForm.respond(interaction, { ephemeral: true });
   } catch (error) {
     // Respond to the user with the error message
     await interaction.reply(
---
+++ src/util/form.ts
@@ -0,0 +1,134 @@
+import {
+  BaseMessageOptions,
+  ButtonStyle,
+  CacheType,
+  CommandInteraction,
+  ComponentType,
+  InteractionReplyOptions,
+  InteractionResponse,
+  StringSelectMenuComponentData,
+} from 'discord.js';
+
+type ResponseInteraction = Awaited<
+  ReturnType<InteractionResponse['awaitMessageComponent']>
+>;
+
+type DiscordFormField = {
+  type: 'select';
+  name: string;
+  placeholder: string;
+  options?: { label: string; value: string }[];
+};
+
+type DiscordFormConfig<TFormValues> = {
+  initialValues: TFormValues;
+  interactionContent?: BaseMessageOptions['content'];
+  fields: DiscordFormField[];
+  onSubmit: (
+    interaction: ResponseInteraction,
+    values: TFormValues,
+  ) => Promise<void>;
+  onCancel?: (interaction: ResponseInteraction) => void;
+  timeout?: number;
+};
+
+const createDiscordForm = <TFormValues>(
+  config: DiscordFormConfig<TFormValues>,
+) => {
+  const render = (values: TFormValues): BaseMessageOptions => {
+    console.log('Values: ', values);
+    return {
+      content: config.interactionContent,
+      components: [
+        ...config.fields.map((field) => {
+          return {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                type: ComponentType.StringSelect,
+                placeholder: field.placeholder,
+                customId: field.name,
+                options: field.options.map((option) => {
+                  return {
+                    ...option,
+                    value: JSON.stringify({
+                      ...values,
+                      [field.name]: option.value,
+                    }),
+                    default: values[field.name] === option.value,
+                  };
+                }),
+              } as StringSelectMenuComponentData,
+            ],
+          };
+        }),
+        {
+          type: ComponentType.ActionRow,
+          components: [
+            {
+              type: ComponentType.Button,
+              label: 'Cancel',
+              style: ButtonStyle.Danger,
+              customId: 'cancel',
+            },
+            {
+              type: ComponentType.Button,
+              label: 'Submit',
+              style: ButtonStyle.Primary,
+              customId: `submit-${JSON.stringify(values)}`,
+            },
+          ],
+        },
+      ],
+    };
+  };
+
+  return {
+    respond: async (
+      interaction: CommandInteraction<CacheType>,
+      options?: InteractionReplyOptions,
+    ) => {
+      const commandResponse = await interaction.reply({
+        ...render(config.initialValues),
+        ...options,
+      });
+
+      const handleResponse = async (
+        response: InteractionResponse,
+      ): Promise<void> => {
+        try {
+          const responseInteraction = await response.awaitMessageComponent({
+            time: 60_000,
+          });
+          if (responseInteraction.isStringSelectMenu()) {
+            const onChangeValues = JSON.parse(responseInteraction.values[0]);
+            const nextResponse = await responseInteraction.update(
+              render(onChangeValues),
+            );
+            await handleResponse(nextResponse);
+          } else if (responseInteraction.customId.startsWith('submit')) {
+            const onSubmitValues = JSON.parse(
+              responseInteraction.customId.replace('submit-', ''),
+            );
+            await config.onSubmit(responseInteraction, onSubmitValues);
+          } else if (responseInteraction.customId === 'cancel') {
+            await responseInteraction.update({
+              content: 'Action cancelled',
+              components: [],
+            });
+          }
+        } catch (error) {
+          console.error(error);
+          await interaction.editReply({
+            content: 'Confirmation not received within 1 minute, cancelling',
+            components: [],
+          });
+        }
+      };
+
+      await handleResponse(commandResponse);
+    },
+  };
+};
+
+export { DiscordFormConfig, createDiscordForm };
