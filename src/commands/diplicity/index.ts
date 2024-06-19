4dad1e61e57e38d8f38382b990add066a53e93b7
= <=>
2024-06-19 12:17:06 +0100

    Add createGame command to index

--- src/commands/diplicity/index.ts
+++ src/commands/diplicity/index.ts
@@ -1,4 +1,5 @@
 export * as createGame from './create-game';
+export * as createOrder from './create-order';
 export * as addMember from './add-member';
 export * as listVariants from './list-variants';
 export * as getMap from './get-map';
