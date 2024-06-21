a39bbadb68db92a95dafd16a2625230958ae3e0b
= <=>
2024-06-21 14:54:48 +0100

    Fix workflow 4

--- scripts/CreateServicePrincipal.ps1
+++ scripts/CreateServicePrincipal.ps1
@@ -2,4 +2,4 @@ param(
     [string]$resourceGroupId
 )
 
-az ad sp create-for-rbac --scope $resourceGroupId --role Contributor --sdk-auth
+az ad sp create-for-rbac --name dipcordServicePrincipal --scope $resourceGroupId --role Contributor --sdk-auth
